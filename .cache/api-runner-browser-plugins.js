53b145b88d9281cee093d014983c40c142a16a77
lizongrong <lizongrong@meican.com>
2022-05-29 20:25:29 +0800

    package

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,4 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-google-gtag/gatsby-browser.js'),
+      options: {"plugins":[],"trackingIds":["G-2K3PH6MKBG"]},
+    }]
---
+++ .cache/sync-requires.js
@@ -0,0 +1,10 @@
+
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---src-pages-index-tsx": (preferDefault(require("/Users/lizongrong/resume/src/pages/index.tsx")))
+}
+
